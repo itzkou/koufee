a61f3adb9e2914722ecf137560db2bb6dbea7445
koutheir elbehi <elbehi.koutheir@gmail.com>
2022-03-24 16:05:10 +0100

    add color hexes

--- app/src/main/java/com/example/koufee/ui/theme/Color.kt
+++ app/src/main/java/com/example/koufee/ui/theme/Color.kt
@@ -2,7 +2,13 @@ package com.example.koufee.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0x201520)
+val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val White =  Color(0xFFFFFFFF)
+val Grege =  Color(0xFFEFE3C8)
+val Brown =  Color(0xFF4A2B29)
+val Black = Color(0xFF201520)
+val RedCoral = Color(0xFFC94C4C)
+val YellowPoopy = Color(0xFFD3A601)
